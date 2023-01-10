1645db0bd47c0d7eee9861c8e874323901295dd6
ArokkiaAntony <antonyvenkat96@gmail.com>
2023-01-11 00:19:47 +0530

    new login functionality

---
+++ src/pull/Push.java
@@ -0,0 +1,14 @@
+package pull;
+
+public class Push {
+	public void Tc1() {
+System.out.println("tony");
+	
+	}
+	public void Tc2() {
+System.out.println("tony");
+}
+	public void Tc3() {
+System.out.println("tony");
+}
+}
